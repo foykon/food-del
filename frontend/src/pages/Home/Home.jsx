54dafca8695542248b7543dcf8ea9dce45fe4bc0
foykon <foykon9@gmail.com>
2024-08-07 14:27:23 +0300

    explore menü not fixed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import { useState } from "react";
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 
 const Home = () => {
