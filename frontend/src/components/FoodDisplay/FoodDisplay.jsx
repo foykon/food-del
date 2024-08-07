bb480bdbc021f5efd882b37c1ef23f12126bdcc7
foykon <foykon9@gmail.com>
2024-08-07 15:24:16 +0300

    BUG FİX

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,8 +11,8 @@ const FoodDisplay = (category) => {
         <h2>Top Dishes near you</h2>
         <div className="food-display-list">
           {food_list.map((item,index)=>{
-            {console.log(category,item.category)}
-            if(category==="All" || category===item.category)
+            {console.log(category)}
+            if(category.category==="All" || category.category===item.category)
               return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
           })}
         </div>
