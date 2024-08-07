39b201b4f1d98e2587cd894f9c8aa965bfb982ff
foykon <foykon9@gmail.com>
2024-08-07 09:32:13 +0300

    implementing storecontext and bug fix => curly brace added

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -6,18 +6,18 @@ import { StoreContext } from '../../context/StoreContext';
 const FoodItem = ({id,name,price,description,image}) => {
   
   const [itemCount,setItemCount] = useState(0);
-  const [cartItems,addToCart,removeFromCart] = useContext(StoreContext);
+  const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
   
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
             <img className='food-item-image' src={image} alt="" />
-            { !itemCount
-              ?<img src={assets.add_icon_white} className='add' onClick={()=>setItemCount(prev=>prev+1)}/>
+            { !cartItems[id]
+              ?<img src={assets.add_icon_white} className='add' onClick={()=>addToCart(id)}/>
               :<div className="food-item-counter">
-                <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-                <p>{itemCount}</p>
-                <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                <p>{cartItems[id]}</p>
+                <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
               </div>
               
             }
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { food_list } from "../assets/assets";
+import { createContext, useState } from 'react';
+import { food_list } from '../assets/assets';
 
 export const StoreContext = createContext(null);
 
