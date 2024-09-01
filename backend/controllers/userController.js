584deb652527e10e8d8ae25b79a480f849506d0c
foykon <foykon9@gmail.com>
2024-09-01 17:22:10 +0300

    user model

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+import userModel from "../models/userModel"
+
+
+// login user
+const loginUser = async (req,res) => {
+
+}
+
+// register user
+const registerUser = async (req,res) => {
+    const {name, password, email}= req.body;
+    try {
+        // checking is user already exists
+        const exists = await userModel.modelName.findOne({email});
+        if(exists){
+            return res.json({success:false, message:"User already exists!"})
+        }
+    } catch (error) {
+        
+    }
+}
+
+export {loginUser, registerUser}
